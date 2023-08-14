398a9032f641fcde9b31b6717f73219fcedc028d
JoanneHelenMana <joannehmana@gmail.com>
2023-08-14 17:08:08 +0800

    Add new feature
    Retrieve password length from user input.

--- password-generator.py
+++ password-generator.py
@@ -2,10 +2,17 @@ import random
 import string
 
 password = []
-length = 10
 
-for i in range(length):
-    randomCharacter = random.choice(string.ascii_letters)
-    password.append(randomCharacter)
+while True:
+    length = int(input(f"What's the required length of your password? Enter a number between 1 and 50: "))
+
+    if 0 < length <= 50:
+        for i in range(length):
+            randomCharacter = random.choice(string.ascii_letters)
+            password.append(randomCharacter)
+        break
+
+    elif length <= 0 or length > 50:
+        print("Number out of range.")
 
 print(f"Password: {''.join(password)}")
